7f6781fd56a99306ce2f54e7c6b239d7fe84dce6
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-31 09:46:33 -0400

    Create Compensation Controller, add GET and POST route; error resolved. constructors/getters/setters defined.

--- src/main/java/com/mindex/challenge/data/Compensation.java
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -7,4 +7,29 @@ public class Compensation {
     private Employee employee;
     private double salary;
     private Date effectiveDate;
+
+    public Compensation(){
+        //default constructor
+    }
+
+    public Compensation(Employee employee, double salary, Date effectiveDate) {
+        this.employee = employee;
+        this.salary = salary;
+        this.effectiveDate = effectiveDate;
+    }
+
+
+    public Employee getEmployee() { return employee; }
+
+    public void setEmployee(Employee employee) { this.employee = employee; }
+
+    public double getSalary() { return salary; }
+
+    public void setSalary(double salary) { this.salary = salary; }
+
+    public Date getEffectiveDate() { return effectiveDate; }
+
+    public void setEffectiveDate(Date effectiveDate) { this.effectiveDate = effectiveDate; }
+
+
 }
