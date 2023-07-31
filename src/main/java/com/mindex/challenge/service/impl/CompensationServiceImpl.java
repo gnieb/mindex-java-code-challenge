f82dd3385ee3ada0f98f8bfdcb9637ece58ad678
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-31 09:13:21 -0400

    Create Compensation Service Impl file and build the two methods used for Commpensation Service

---
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -0,0 +1,26 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.service.CompensationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompensationServiceImpl implements CompensationService {
+
+    @Autowired
+    private CompensationRepository compensationRepository;
+
+    @Override
+    public Compensation createCompensation(Compensation compensation) {
+        return compensationRepository.save(compensation);
+    };
+
+
+    @Override
+    public Compensation readCompensationByEmployeeId(String emId) {
+        return compensationRepository.findByEmployee_EmployeeId(emId);
+    }
+
+}
