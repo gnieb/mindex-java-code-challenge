1102b6bedfc121e559020716c01a920a7ec58bfe
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-30 22:14:31 -0400

    Create Compensation.java entity

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,10 @@
+package com.mindex.challenge.data;
+
+import java.util.Date;
+
+public class Compensation {
+
+    private Employee employee;
+    private double salary;
+    private Date effectiveDate;
+}
